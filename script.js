8985b9bc39dd40f4313017698c508e9cb6eff554
magda-u <magdalena.urzon@gmail.com>
2019-04-08 10:06:06 +0200

    add progress circle

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let addNumber;
 let index = 0;
 
+
 // create new array for each circle
 
 const Array = function () {
@@ -30,19 +31,24 @@ circleThree.addNumber(80)
 
 //----------------------------------------
 
-const letCount = () => {
+const letCountOne = () => {
     let numberOne = document.querySelector('.circle1');
-    let numberTwo = document.querySelector('.circle2');
-    let numberThree = document.querySelector('.circle3');
     if (index < circleOne.tab.length) {
         numberOne.innerHTML = circleOne.tab[index];
         index++
     }
+}
 
+const letCountTwo = () => {
+    let numberTwo = document.querySelector('.circle2');
     if (index < circleTwo.tab.length) {
         numberTwo.innerHTML = circleTwo.tab[index];
         index++
     }
+}
+
+const letCountThree = () => {
+    let numberThree = document.querySelector('.circle3');
     if (index < circleThree.tab.length) {
         numberThree.innerHTML = circleThree.tab[index];
         index++
@@ -51,10 +57,20 @@ const letCount = () => {
 
 
 function addInterval(e) {
-    if (e.target.documentElement.scrollTop > 100) {
-        addNumber = setInterval(letCount, 200)
+    if (e.target.documentElement.scrollTop > 80) {
+        addNumber = setInterval(letCountOne, 200);
+        document.querySelector('.circle_mask1').classList.add('circle1_active');
     }
 
+    if (e.target.documentElement.scrollTop > 180) {
+        addNumber = setInterval(letCountTwo, 200);
+        document.querySelector('.circle_mask2').classList.add('circle2_active');
+    }
+
+    if (e.target.documentElement.scrollTop > 280) {
+        addNumber = setInterval(letCountThree, 200);
+        document.querySelector('.circle_mask3').classList.add('circle3_active');
+    }
 }
 
 window.addEventListener('scroll', addInterval);
